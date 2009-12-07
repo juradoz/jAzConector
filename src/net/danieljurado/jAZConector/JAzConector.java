f4315aedc55c5a6944595cd578869d1fe627ac4c
Daniel Jurado <jurado@gmail.com>
2009-12-07 21:33:23 -0200

    Atualizada para versao 1.0.5.
    Signed-off-by: Daniel Jurado <jurado@gmail.com>

--- src/net/danieljurado/jAZConector/JAzConector.java
+++ src/net/danieljurado/jAZConector/JAzConector.java
@@ -256,9 +256,9 @@ public class JAzConector extends Applet {
 	public static final int EV_MAKECALLCONF = 5;
 
 	/**
-	 * Versao atual do componente: 1.0.4
+	 * Versao atual do componente: 1.0.5
 	 */
-	public static final String VERSION = "1.0.4";
+	public static final String VERSION = "1.0.5";
 
 	private String agentStateToStr(int agentState) {
 		switch (agentState) {
